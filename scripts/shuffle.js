8583813453ae881fa89322374ad077bbcf08c8e7
Acrilix <dev@acrilix.net>
2015-02-17 08:53:12 +0000

    initial files for the puzzle itself

---
+++ scripts/shuffle.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+alert("Test");
+
+$("#parrot").click(function() {
+    alert("Test");
+    alert("You've chosen the parrot puzzle.");
+});
+
+
+
+
+
