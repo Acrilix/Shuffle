628dacab7e1ab8739dfcbd7422bc91bf8fa1e11f
Acrilix <dev@acrilix.net>
2015-02-17 17:45:50 +0000

    started to flesh out how the puzzle will work.

--- scripts/shuffle.js
+++ scripts/shuffle.js
@@ -4,12 +4,57 @@
  * and open the template in the editor.
  */
 
-alert("Test");
+//alert("Test");
 
 $("#parrot").click(function() {
-    alert("Test");
-    alert("You've chosen the parrot puzzle.");
+    prepareThePuzzle("parrot");
 });
+$("#cat").click(function() {
+    prepareThePuzzle("cat");
+});
+$("#moon").click(function() {
+    prepareThePuzzle("moon");
+});
+$("#rockies").click(function() {
+    prepareThePuzzle("rockies");
+});
+
+$( "#test" ).click(function() {
+    $( this ).slideUp();
+});
+
+function prepareThePuzzle(puzzleName)
+{
+    alert("You've chosen the " + puzzleName + " puzzle.");
+    
+    var imgPath = "";
+    var puzzlePieces = []; //16 pieces initially
+    
+    switch (puzzleName)
+    {
+        case "parrot":
+            imgPath = "img/parrot/";
+            break;
+        case "cat":
+            imgPath = "img/cat/";
+            break;
+        case "moon":
+            imgPath = "img/moon/";
+            break;
+        case "rockies":
+            imgPath = "img/rockies/";
+            break;
+    }
+    
+    //get the parrot puzzle pieces.
+    
+    //display them in order in the puzzle grid.
+    
+    //shuffle the pieces until all items have moved at least once.
+    
+    //start the clock running
+    
+}
 
 
 
