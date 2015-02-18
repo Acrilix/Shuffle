bfe9264c4ad87511929e2f65c9de30eca79dbee6
Acrilix <dev@acrilix.net>
2015-02-18 17:49:07 +0000

    continued to layout the various elements of the puzzle interface.

--- scripts/shuffle.js
+++ scripts/shuffle.js
@@ -5,6 +5,8 @@
  */
 
 //alert("Test");
+var puzzleGrid = []; //16 squares
+var puzzlePieces = []; //15 pieces
 
 $("#parrot").click(function() {
     prepareThePuzzle("parrot");
@@ -28,7 +30,10 @@ function prepareThePuzzle(puzzleName)
     alert("You've chosen the " + puzzleName + " puzzle.");
     
     var imgPath = "";
-    var puzzlePieces = []; //16 pieces initially
+    puzzleGrid = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
+    
+    alert(puzzleGrid[0][0]);
+    alert(puzzleGrid[3][3]);
     
     switch (puzzleName)
     {
@@ -47,6 +52,7 @@ function prepareThePuzzle(puzzleName)
     }
     
     //get the parrot puzzle pieces.
+    puzzlePieces = loadPuzzle(puzzlePieces);
     
     //display them in order in the puzzle grid.
     
@@ -54,6 +60,48 @@ function prepareThePuzzle(puzzleName)
     
     //start the clock running
     
+}
+
+function loadPuzzle(puzzlePieces)
+{
+    //prepare the array
+    
+    var rowCount = 0;
+    var colCount = 0;
+    var pieceCount = 0;
+    
+    //loop through each row
+    for	(c = 0; c < 4; c++) {
+        //alert("row:" + (r+1));
+        
+        //loop through each column
+        for(r = 0; r < 4; r++)
+        {
+            //alert("col:" + (c+1));
+            
+            if(r == 0 && c == 0)
+            {
+                //don't put anything in the first cell when first display the puzzle.
+                puzzlePieces[pieceCount] = [r,c]; //piece 0 is the empty cell!
+            }
+            else
+            {
+                //alert(pieceCount);
+            
+                puzzlePieces[pieceCount] = [r,c];
+            
+                //testing, display the piece No. in the div tag.
+                $("#cell" + (pieceCount + 1)).html(pieceCount);
+            }
+            
+            pieceCount++;
+            colCount++;
+        }
+        
+        rowCount++;
+    }
+  
+    return puzzlePieces;
 }
 
 
